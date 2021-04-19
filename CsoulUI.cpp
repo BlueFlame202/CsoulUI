79d8f6c9336c6274bb19f60069ac6f0d51633bbd
Aathreya Kadambi <aathreyakadambi@gmail.com>
2021-04-19 06:08:44 +0000

    commented

--- CsoulUI.cpp
+++ CsoulUI.cpp
@@ -3,6 +3,8 @@
 CsoulUI::CsoulUI(std::string title, std::string ver)
 {
   std::cout << "[CsoulUI]: Initializing " << title << " " << ver << std::endl;
+  
+  // define defaults so that if they get overwritten we get a warning, and also so that we can have a nice description
   m_commandMap["HELP"] = std::make_pair("displays a list of commands", [](std::string s) { return; });
   m_commandMap["SOULSLEEP"] = std::make_pair("ends CSoulUI", [](std::string s) { return; });
 }
@@ -27,7 +29,7 @@ std::string CsoulUI::help()
 
 void CsoulUI::createCommand(std::string s, std::string info, std::function<void(std::string)> f)
 {
-  if (m_commandMap[s].second)
+  if (m_commandMap[s].second) // check if it already exists and simultaneously allocate the memory if it doesnt
     std::cout << "[CsoulUI]: WARNING! Command " << s << " is being overwritten!";
   m_commandMap[s] = std::make_pair(info, f);
 }
@@ -35,7 +37,7 @@ void CsoulUI::createCommand(std::string s, std::string info, std::function<void(
 void CsoulUI::listen()
 {
   std::string command;
-  std::getline(std::cin, command);
+  std::getline(std::cin, command); // wait for and get user input
   int temp = command.find(' ');
   if (temp != -1)
     respond(command.substr(0, temp), command.substr(temp + 1));
@@ -46,7 +48,7 @@ void CsoulUI::listen()
 void CsoulUI::respond(std::string command, std::string args)
 {
   if (command == "HELP") std::cout << help() << std::endl;
-  else if (command == "SOULSLEEP") return;
+  else if (command == "SOULSLEEP") return; // break out of the listen-respond loop
   else if (!m_commandMap[command].second) 
     std::cout << "[CsoulUI]: ERROR! Command " << command << " not recognized." << std::endl;
   else
@@ -55,5 +57,5 @@ void CsoulUI::respond(std::string command, std::string args)
     m_commandMap[command].second(args);
     std::cout << std::endl;
   }
-  listen();
+  listen(); // loop, we keep listening and responding again and again
 }
--- CsoulUI.h
+++ CsoulUI.h
@@ -11,18 +11,19 @@ private:
   std::string m_title; // title of the program using CSoulUI (primary program)
   std::string m_ver; // version of the primary program
 
-  std::unordered_map<std::string, std::pair<std::string, std::function<void(std::string)>>> m_commandMap; // stores function names, info and corresponding functions
+  std::unordered_map<std::string, std::pair<std::string, std::function<void(std::string)>>> m_commandMap; // stores function names, info, and corresponding functions
 
 public:
-  CsoulUI(std::string title, std::string ver);
+  CsoulUI(std::string title, std::string ver); // constructor
 
-  std::string getTitle();
-  std::string getVersion();
+  // accessors
+  std::string getTitle(); // for m_title
+  std::string getVersion(); // for m_ver
 
-  std::string help();
+  std::string help(); // get a list of commands with descriptions
 
-  void createCommand(std::string s, std::string info, std::function<void(std::string)> f);
+  void createCommand(std::string s, std::string info, std::function<void(std::string)> f); // create a command
 
-  void listen();
-  void respond(std::string command, std::string args);
+  void listen(); // wait for user input and process it for respond
+  void respond(std::string command, std::string args); // respond and listen again
 };
