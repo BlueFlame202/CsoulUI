5c9451d34fc81b4fcfb7656637db8f9eee4ac389
Aathreya Kadambi <aathreyakadambi@gmail.com>
2021-04-19 05:47:17 +0000

    Fixed indenting to make it look nice on Github.

--- CsoulUI.h
+++ CsoulUI.h
@@ -8,21 +8,21 @@
 class CsoulUI
 {
 private:
-	std::string m_title;
-	std::string m_ver;
+  std::string m_title;
+  std::string m_ver;
 
-	std::unordered_map<std::string, std::function<void(std::string)>> m_commandMap;
+  std::unordered_map<std::string, std::function<void(std::string)>> m_commandMap;
 
 public:
-	CsoulUI(std::string title, std::string ver);
+  CsoulUI(std::string title, std::string ver);
 
   std::string getTitle();
   std::string getVersion();
 
-	std::string help();
+  std::string help();
 
-	void createCommand(std::string s, std::function<void(std::string)> f);
+  void createCommand(std::string s, std::function<void(std::string)> f);
 
-	void listen();
-	void respond(std::string command, std::string args);
+  void listen();
+  void respond(std::string command, std::string args);
 };
